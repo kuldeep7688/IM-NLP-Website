2d650d32e1b8689ef1ba5ddb6172f871c8379e6c
kuldeep7688 <7688kuldeepsingh@gmail.com>
2024-07-01 23:20:43 -0400

    added authentication using jwt_tokens

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+
+
+# user model  already provided by django
+
+# model predictions and user inputs model 
